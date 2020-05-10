dd8c57964d3bcd9ff1a358a36cfd53cf84b4f8c5
Radek Burget <burgetr@fit.vutbr.cz>
2020-05-10 20:03:17 +0200

    GUI tools moved to a separate FitLayoutGUI package.

--- fitlayout-tools/src/main/java/module-info.java
+++ fitlayout-tools/src/main/java/module-info.java
@@ -1,13 +1,17 @@
 module cz.vutbr.fit.layout.tools 
 {
     requires java.xml;
-    requires java.desktop;
     requires java.scripting;
     requires org.slf4j;
     
-    requires cz.vutbr.fit.layout.core;
+    requires transitive cz.vutbr.fit.layout.core;
     requires cz.vutbr.fit.layout.cssbox;
     requires cz.vutbr.fit.layout.segm;
+    
+    exports cz.vutbr.fit.layout.process;
+    exports cz.vutbr.fit.layout.process.js;
+    exports cz.vutbr.fit.layout.tools;
+    exports cz.vutbr.fit.layout.tools.io;
     
     requires jline;
 }

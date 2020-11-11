54083b2257cfd5a9c92685dea1fefef026ebcac2
Radek Burget <burgetr@fit.vutbr.cz>
2020-11-11 14:30:46 +0100

    Puppeteer renderer added to dependencies

--- fitlayout-tools/src/main/java/module-info.java
+++ fitlayout-tools/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module cz.vutbr.fit.layout.tools
     
     requires transitive cz.vutbr.fit.layout.core;
     requires cz.vutbr.fit.layout.cssbox;
+    requires cz.vutbr.fit.layout.puppeteer;
     requires cz.vutbr.fit.layout.segm;
     
     exports cz.vutbr.fit.layout.process;

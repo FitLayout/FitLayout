5e62d23159612f1f196ac893de14efc165fd2639
Radek Burget <burgetr@fit.vutbr.cz>
2020-11-19 21:14:57 +0100

    Reuse original toString() in RDFBox

--- fitlayout-storage-rdf/src/main/java/cz/vutbr/fit/layout/rdf/model/RDFBox.java
+++ fitlayout-storage-rdf/src/main/java/cz/vutbr/fit/layout/rdf/model/RDFBox.java
@@ -8,7 +8,6 @@ package cz.vutbr.fit.layout.rdf.model;
 import org.eclipse.rdf4j.model.IRI;
 
 import cz.vutbr.fit.layout.impl.DefaultBox;
-import cz.vutbr.fit.layout.model.Box;
 
 /**
  * 
@@ -37,31 +36,4 @@ public class RDFBox extends DefaultBox implements RDFResource
         setId(Integer.parseInt(uri.getLocalName()));
     }
 
-    @Override
-    public String toString()
-    {
-        String ret = getId() + " ";
-        if (getType() == Box.Type.TEXT_CONTENT)
-        {
-            ret += getText();
-        }
-        else if (getType() == Box.Type.REPLACED_CONTENT)
-        {
-            if (getContentObject() != null)
-                ret += getContentObject().toString();
-            else
-                ret += "(null object)";
-        }
-        else
-        {
-            ret += "<" + getTagName();
-            if (getAttribute("id") != null)
-                ret += " id=" + getAttribute("id");
-            if (getAttribute("class") != null)
-                ret += " class=" + getAttribute("class");
-            ret += ">";
-        }
-        return ret;
-    }
-    
 }

b0bbf1cbbe02fa1970b2eb634323ca32e19212b7
Radek Burget <burgetr@fit.vutbr.cz>
2021-02-12 13:07:31 +0100

    Multi-command support in cli

--- fitlayout-tools/src/main/java/cz/vutbr/fit/layout/tools/Cli.java
+++ fitlayout-tools/src/main/java/cz/vutbr/fit/layout/tools/Cli.java
@@ -5,6 +5,10 @@
  */
 package cz.vutbr.fit.layout.tools;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
@@ -16,18 +20,45 @@ import picocli.CommandLine.Command;
 public class Cli
 {
 
+    private static List<List<String>> splitArgsByCommands(String[] args, Set<String> cnames)
+    {
+        List<List<String>> ret = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        ret.add(current);
+        for (int i = 0; i < args.length; i++)
+        {
+            if (cnames.contains(args[i]) && !current.isEmpty())
+            {
+                current = new ArrayList<>();
+                ret.add(current);
+            }
+            current.add(args[i]);
+        }
+        return ret;
+    }
+    
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        //Renderer renderer = new Renderer();
-        
         CommandLine cmd = new CommandLine(new Cli());
         cmd.setUsageHelpWidth(90);
         cmd.setUsageHelpLongOptionsMaxWidth(40);
-        int exitCode = cmd.execute(args);
-        System.exit(exitCode);
+        System.out.println(cmd.getCommandSpec().userObject());
+        
+        //split command line to individual commands
+        Set<String> cnames = cmd.getSubcommands().keySet();
+        List<List<String>> subcommands = splitArgsByCommands(args, cnames);
+        
+        //execute the individual command lines
+        for (List<String> subcl : subcommands)
+        {
+            String[] a = subcl.toArray(new String[0]);
+            int exitCode = cmd.execute(a);
+            if (exitCode != 0)
+                System.exit(exitCode);
+        }
     }
 
 }

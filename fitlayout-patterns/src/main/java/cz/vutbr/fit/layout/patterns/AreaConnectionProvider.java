b9584580827c1a53a59a9c0dc4fe2ad583ca6731
Radek Burget <burgetr@fit.vutbr.cz>
2021-12-27 19:40:27 +0100

    Connection set providers added

---
+++ fitlayout-patterns/src/main/java/cz/vutbr/fit/layout/patterns/AreaConnectionProvider.java
@@ -0,0 +1,121 @@
+/**
+ * AreaConnectionProvider.java
+ *
+ * Created on 26. 12. 2021, 22:18:40 by burgetr
+ */
+package cz.vutbr.fit.layout.patterns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.rdf4j.model.IRI;
+
+import cz.vutbr.fit.layout.api.Parameter;
+import cz.vutbr.fit.layout.api.ServiceException;
+import cz.vutbr.fit.layout.impl.BaseArtifactService;
+import cz.vutbr.fit.layout.model.Area;
+import cz.vutbr.fit.layout.model.AreaTree;
+import cz.vutbr.fit.layout.model.Artifact;
+import cz.vutbr.fit.layout.model.ConnectionSet;
+import cz.vutbr.fit.layout.model.ContentRect;
+import cz.vutbr.fit.layout.model.Page;
+import cz.vutbr.fit.layout.ontology.BOX;
+import cz.vutbr.fit.layout.ontology.SEGM;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class AreaConnectionProvider extends BaseArtifactService
+{
+
+    public AreaConnectionProvider()
+    {
+    }
+
+    @Override
+    public String getId()
+    {
+        return "FitLayout.AreaConnections";
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Area connection extractor";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Extracts various connections between visual areas.";
+    }
+
+    @Override
+    public List<Parameter> defineParams()
+    {
+        List<Parameter> ret = new ArrayList<>(1);
+        return ret;
+    }
+
+    @Override
+    public IRI getConsumes()
+    {
+        return SEGM.AreaTree;
+    }
+
+    @Override
+    public IRI getProduces()
+    {
+        return BOX.ConnectionSet;
+    }
+
+    @Override
+    public Artifact process(Artifact input) throws ServiceException
+    {
+        if (input != null && input instanceof AreaTree)
+        {
+            AreaTree atree = (AreaTree) input;
+            if (atree.getPageIri() != null)
+            {
+                Artifact page = getServiceManager().getArtifactRepository().getArtifact(atree.getPageIri());
+                if (page != null && page instanceof Page)
+                    return extractConnections(atree, (Page) page);
+                else
+                    throw new ServiceException("Couldn't fetch source page");
+            }
+            else
+                throw new ServiceException("Source page not set");
+        }
+        else
+            throw new ServiceException("Source artifact not specified or not an area tree");
+    }
+
+    public ConnectionSet extractConnections(AreaTree input, Page page)
+    {
+        List<ContentRect> leafAreas = new ArrayList<>();
+        findLeafAreas(input.getRoot(), leafAreas);
+        
+        RelationAnalyzer ra = new RelationAnalyzerSymmetric(page, leafAreas);
+        ra.extractConnections();
+        
+        DefaultConnectionSet ret = new DefaultConnectionSet(input.getIri());
+        ret.setAreaConnections(ra.getConnections());
+        ret.setPageIri(page.getIri());
+        return ret;
+    }
+    
+    private void findLeafAreas(Area root, List<ContentRect> areas)
+    {
+        if (root.isLeaf())
+            areas.add(root);
+        else
+        {
+            for (Area child : root.getChildren())
+            {
+                findLeafAreas(child, areas);
+            }
+        }
+    }
+    
+}
---
+++ fitlayout-patterns/src/main/java/cz/vutbr/fit/layout/patterns/TextChunkConnectionProvider.java
@@ -0,0 +1,108 @@
+/**
+ * TextChunkConnectionProvider.java
+ *
+ * Created on 26. 12. 2021, 22:23:34 by burgetr
+ */
+package cz.vutbr.fit.layout.patterns;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.rdf4j.model.IRI;
+
+import cz.vutbr.fit.layout.api.Parameter;
+import cz.vutbr.fit.layout.api.ServiceException;
+import cz.vutbr.fit.layout.impl.BaseArtifactService;
+import cz.vutbr.fit.layout.model.Artifact;
+import cz.vutbr.fit.layout.model.ChunkSet;
+import cz.vutbr.fit.layout.model.ConnectionSet;
+import cz.vutbr.fit.layout.model.ContentRect;
+import cz.vutbr.fit.layout.model.Page;
+import cz.vutbr.fit.layout.ontology.BOX;
+import cz.vutbr.fit.layout.ontology.SEGM;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class TextChunkConnectionProvider extends BaseArtifactService
+{
+
+    public TextChunkConnectionProvider()
+    {
+    }
+
+    @Override
+    public String getId()
+    {
+        return "FitLayout.TextChunkConnections";
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Text chunk connection extractor";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "Extracts various connections between text chunks.";
+    }
+
+    @Override
+    public List<Parameter> defineParams()
+    {
+        List<Parameter> ret = new ArrayList<>(1);
+        return ret;
+    }
+
+    @Override
+    public IRI getConsumes()
+    {
+        return SEGM.TextChunk;
+    }
+
+    @Override
+    public IRI getProduces()
+    {
+        return BOX.ConnectionSet;
+    }
+
+    @Override
+    public Artifact process(Artifact input) throws ServiceException
+    {
+        if (input != null && input instanceof ChunkSet)
+        {
+            ChunkSet cset = (ChunkSet) input;
+            if (cset.getPageIri() != null)
+            {
+                Artifact page = getServiceManager().getArtifactRepository().getArtifact(cset.getPageIri());
+                if (page != null && page instanceof Page)
+                    return extractConnections(cset, (Page) page);
+                else
+                    throw new ServiceException("Couldn't fetch source page");
+            }
+            else
+                throw new ServiceException("Source page not set");
+        }
+        else
+            throw new ServiceException("Source artifact not specified or not a chunk set");
+    }
+
+    public ConnectionSet extractConnections(ChunkSet input, Page page)
+    {
+        Set<ContentRect> chunks = new HashSet<>(input.getTextChunks());
+        RelationAnalyzer ra = new RelationAnalyzerSymmetric(page, chunks);
+        ra.extractConnections();
+        
+        DefaultConnectionSet ret = new DefaultConnectionSet(input.getIri());
+        ret.setAreaConnections(ra.getConnections());
+        ret.setPageIri(page.getIri());
+        return ret;
+    }
+    
+
+}

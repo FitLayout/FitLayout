9a15d6d3beb2afc88e19e159e2fa1b82e26330bf
Radek Burget <burgetr@fit.vutbr.cz>
2022-01-30 21:21:43 +0100

    Parallel batch iteration

--- fitlayout-tools/src/main/java/cz/vutbr/fit/layout/tools/cmd/Batch.java
+++ fitlayout-tools/src/main/java/cz/vutbr/fit/layout/tools/cmd/Batch.java
@@ -5,10 +5,16 @@
  */
 package cz.vutbr.fit.layout.tools.cmd;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import cz.vutbr.fit.layout.tools.CliCommand;
 import cz.vutbr.fit.layout.tools.util.ArgumentTokenizer;
@@ -30,8 +36,11 @@ public class Batch extends CliCommand implements Callable<Integer>
     @Option(order = 1, names = {"-i", "--data"}, description = "Input data file to iterate on")
     protected String inFile;
     
-    @Option(order = 2, names = {"-p", "--threads"}, description = "Number of threads to use for iteration (default 1)")
-    protected int threads;
+    @Option(order = 2, names = {"-d", "--delemiter"}, description = "Data field delimiter in the input data file (default \\\t)")
+    protected String delim = "\\t";
+    
+    @Option(order = 3, names = {"-p", "--threads"}, description = "Number of threads to use for iteration (default 1)")
+    protected int threads = 1;
     
     @Parameters(arity = "1", index = "0", paramLabel = "batch_file", description = "A text file containing commands to execute")
     protected String batchFile;
@@ -39,15 +48,110 @@ public class Batch extends CliCommand implements Callable<Integer>
     @Override
     public Integer call() throws Exception
     {
-        String cmdString = Files.readString(Path.of(batchFile));
-        return execCommandLine(cmdString);
+        try {
+            String cmdString = Files.readString(Path.of(batchFile));
+            if (inFile == null)
+                return execCommandLine(cmdString, null);
+            else
+                return iterateDataFile(cmdString);
+        } catch (IOException e) {
+            return 1;
+        }
     }
 
-    private int execCommandLine(String cmdString)
+    private int execCommandLine(String cmdString, String dataLine)
     {
+        if (dataLine != null)
+        {
+            // replace $? with data fileds fields when provided
+            String[] fields = dataLine.split(delim);
+            for (int i = 0; i < fields.length; i++)
+            {
+                cmdString = cmdString.replaceAll("\\$"+(i+1), fields[i]);
+            }
+            cmdString = cmdString.replaceAll("\\$0", dataLine);
+        }
         List<String> cmdList = ArgumentTokenizer.tokenize(cmdString);
         String[] args = cmdList.toArray(new String[0]);
         return getCli().execCommandLine(args);
+    }
+    
+    private int iterateDataFile(String cmdString) throws IOException
+    {
+        List<BatchTask> tasks = createTasks(inFile, cmdString);
+        ExecutorService exec = Executors.newFixedThreadPool(threads);
+        try
+        {
+            List<Future<Integer>> results = exec.invokeAll(tasks);
+            for (Future<Integer> ft : results)
+            {
+                ft.get();
+            }
+            return 0;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return 1;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return 1;
+        } finally {
+            exec.shutdown();
+        }
+    }
+
+    private List<BatchTask> createTasks(String listFile, String cmdString)
+            throws IOException
+    {
+        Path filePath = Path.of(listFile);
+        List<String> dataLines = Files.readAllLines(filePath);
+        List<BatchTask> tasks = new LinkedList<>();
+        long index = 0;
+        for (String dataLine : dataLines)
+            tasks.add(new BatchTask(this, index++, cmdString, dataLine));
+        return tasks;
+    }
+    
+    public void taskFinished(BatchTask task, int status)
+    {
+        System.err.print((status == 0) ? "Done: " : "ERROR: ");
+        System.err.println("(" + task.getIndex() + ") " + task.getDataLine());
+    }
+    
+    // ===============================================================
+    
+    private static class BatchTask implements Callable<Integer>
+    {
+        private long index;
+        private Batch parent;
+        private String cmdString;
+        private String dataLine;
+        
+        public BatchTask(Batch parent, long index, String cmdString, String dataLine)
+        {
+            this.index = index;
+            this.parent = parent;
+            this.cmdString = cmdString;
+            this.dataLine = dataLine;
+        }
+
+        public long getIndex()
+        {
+            return index;
+        }
+
+        public String getDataLine()
+        {
+            return dataLine;
+        }
+
+        @Override
+        public Integer call() throws Exception
+        {
+            int ret = parent.execCommandLine(cmdString, dataLine);
+            parent.taskFinished(this, ret);
+            return ret;
+        }
+        
     }
     
 }
